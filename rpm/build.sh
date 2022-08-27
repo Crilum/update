fc01ec9c5c9fb8a6c751d44fadcd0bc649babfb6
Crilum <91354257+Crilum@users.noreply.github.com>
2022-08-27 15:28:03 -0600

    Update build.sh

--- rpm/build.sh
+++ rpm/build.sh
@@ -6,7 +6,7 @@ declare -r NAME="update"
 declare -r BUILD_DIR="$(pwd)"
 declare -r VERSION="$(grep "version=" "${BUILD_DIR}/${NAME}" | cut -d"\"" -f2 | cut -d"v" -f2)"
 
-echo "name: $NAME, build dir: $BUILD_DIR, version: $version"
+echo "name: $NAME, build dir: $BUILD_DIR, version: $version, Dir contents:"; ls
 
 if command -v dnf &>/dev/null; then
    command -v rpmdev-setuptree &> /dev/null && command -v rpmbuild &>/dev/null || sudo dnf install rpm-build rpmdevtools -y
