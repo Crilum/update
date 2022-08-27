5faa62336860f657be3ce4185011bae22eb9d5bf
Anton <vanasscheanton@gmail.com>
2022-08-27 17:05:14 +0200

    Add script to build rpm packages (#17)
    * Add spec file
    
    * Add build script for rpm packages
    
    * Add build instructions
    
    * Remove temporary variables
    
    * Update build instructions
    
    * Add support for CentOS and Debian based systems
    
    * Remove `rpmdev-setuptree` package installed check
    
    Thank you, @AntonVanAssche!

---
+++ rpm/REAMDE.md
@@ -0,0 +1,8 @@
+# Building the RPM package
+
+To build an rpm package, run the following command from the core of the repository.
+
+```
+$ ./rpm/build.sh
+```
+
---
+++ rpm/build.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# This script will package the `update` script into a `rpm` package.
+
+declare -r NAME="update"
+declare -r BUILD_DIR="$(pwd)"
+declare -r VERSION="$(grep "version=" "${BUILD_DIR}/${NAME}" | cut -d"\"" -f2 | cut -d"v" -f2)"
+
+if command -v dnf &>/dev/null; then
+   command -v rpmdev-setuptree &> /dev/null && command -v rpmbuild &>/dev/null || sudo dnf install rpm-build rpmdevtools -y
+elif command -v yum &>/dev/null; then
+   command -v rpmdev-setuptree &> /dev/null && command -v rpmbuild &>/dev/null || sudo yum install rpm-build rpmdevtools -y
+elif command -v apt &>/dev/null; then
+   command -v rpmbuild &>/dev/null || sudo apt install rpm -y
+else
+   printf 'No suitable package manager found!! Exiting...'
+   exit 1
+fi
+
+# Exit out when running the script as root.
+if [[ ${UID} -eq 0 ]]; then
+   printf 'Please run this script as a normal user.'
+   exit 1
+fi
+
+# Create the necessary directories.
+mkdir -p "${HOME}/rpmbuild/"{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+mkdir -p "${HOME}/rpmbuild/BUILD/${NAME}-${VERSION}"
+mkdir -p "${BUILD_DIR}/${NAME}-${VERSION}"
+
+# Create the tarball.
+cp -r "${BUILD_DIR}/${NAME}" "${BUILD_DIR}/${NAME}-${VERSION}"
+tar --create --file "${NAME}-${VERSION}.tar.gz" "${NAME}-${VERSION}"
+
+# Move the tarball to the SOURCES directory.
+mv "${BUILD_DIR}/${NAME}-${VERSION}.tar.gz" "${HOME}/rpmbuild/SOURCES/"
+
+# Update the version number.
+sed -i "s/VERSION THE SCRIPT WILL UPDATE THIS!/${VERSION}/g" "${BUILD_DIR}/rpm/${NAME}.spec"
+
+# Copy over the spec file.
+cp -r "${BUILD_DIR}/rpm/${NAME}.spec" "${HOME}/rpmbuild/SPECS/${NAME}.spec"
+
+# build RPM package.
+rpmbuild -ba "${HOME}/rpmbuild/SPECS/${NAME}.spec"
+
+# Cleanup
+rm -rf "${NAME}-${VERSION}"
+
